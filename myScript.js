48053fa3d35365be987c1be2bcca538fb3ea91d8
Erick Toledo <erick.toledo@msg.com>
2017-11-01 00:05:25 -0400

    practice

---
+++ myScript.js
@@ -0,0 +1,20 @@
+function myFunction() {
+   document.getElementById("1stParagraph").innerHTML = "Paragraph changed bitches.";
+}
+
+
+function pg13() {
+	document.getElementById("1stParagraph").innerHTML = "That wasnt very nice";
+}
+
+function htmloutput () {
+	document.getElementById("").document.write = "HTMLOUTPUT!!!!";
+}
+
+function alertbox () {
+        document.getElementById("2ndDemo").window.alert = "HEREISAWINDOW";
+}
+
+function browserconsole () {
+        document.getElementById("2ndDemo").console.log = "CONSOLE";
+}
